834c9f504d96402d4f7f9703b4dd1e43713072e5
Matt Moran <15150339+DarkMatterMatt@users.noreply.github.com>
2020-03-23 13:31:44 +1300

    Add browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -5,15 +5,26 @@ const ts = require("gulp-typescript");
 const sass = require("gulp-sass");
 const sourcemaps = require("gulp-sourcemaps");
 const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
 const tsConfig = require("./tsconfig.json");
+
+gulp.task("serve", () => {
+    browserSync.init({
+        server: {
+            baseDir: "./dist",
+        },
+    });
+});
 
 gulp.task("images", () => gulp
     .src("src/images/*")
-    .pipe(gulp.dest("dist/images")));
+    .pipe(gulp.dest("dist/images"))
+    .pipe(browserSync.reload({ stream: true })));
 
 gulp.task("html", () => gulp
     .src("src/html/*.html")
-    .pipe(gulp.dest("dist")));
+    .pipe(gulp.dest("dist"))
+    .pipe(browserSync.reload({ stream: true })));
 
 gulp.task("scss", () => gulp
     .src("src/scss/*.scss")
@@ -21,14 +32,16 @@ gulp.task("scss", () => gulp
     .pipe(sass())
     .pipe(autoprefixer({ cascade: false }))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist")));
+    .pipe(gulp.dest("dist"))
+    .pipe(browserSync.reload({ stream: true })));
 
 gulp.task("ts", () => gulp
     .src("src/ts/*.ts")
     .pipe(sourcemaps.init())
     .pipe(ts(tsConfig.compilerOptions))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest("dist")));
+    .pipe(gulp.dest("dist"))
+    .pipe(browserSync.reload({ stream: true })));
 
 gulp.task("watch", () => {
     gulp.watch("src/images/*", gulp.series("images"));
@@ -37,4 +50,4 @@ gulp.task("watch", () => {
     gulp.watch("src/ts/*.ts", gulp.series("ts"));
 });
 
-exports.default = gulp.parallel("images", "html", "scss", "ts", "watch");
+exports.default = gulp.parallel("serve", "images", "html", "scss", "ts", "watch");
