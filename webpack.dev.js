2d68ff842ed0d47a10a138b2182999f04094008e
DarkMatterMatt <mattmoran2000@gmail.com>
2021-01-15 10:12:12 +1300

    Automatically pick unused port for dev server, default to localhost if no other iface detected

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,107 +1,113 @@
 const os = require("os");
 const path = require("path");
 const Dotenv = require("dotenv-webpack");
+const getPort = require("get-port");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const WorkboxPlugin = require("workbox-webpack-plugin");
 
-// find internal IP
-const ifaces = os.networkInterfaces();
-let iface = ifaces["Ethernet"] || ifaces["Wi-Fi"] || ifaces["eth0"] || ifaces["wlan0"];
-iface = iface && iface.find(i => i.family === "IPv4");
-const ip = iface && iface.address;
+module.exports = (async () => {
+    // find internal IP
+    const ifaces = os.networkInterfaces();
+    let iface = ifaces["Ethernet"] || ifaces["Wi-Fi"] || ifaces["eth0"] || ifaces["wlan0"];
+    iface = iface && iface.find(i => i.family === "IPv4");
+    const ip = iface ? iface.address : "127.0.0.1";
 
-module.exports = {
-    entry: {
-        main: "./src/ts/index.ts",
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"],
-    },
-    output: {
-        path:     path.resolve(__dirname, "dist"),
-        filename: "[name].[chunkhash].js",
-        publicPath: "",
-    },
-    devtool:   "source-map",
-    devServer: {
-        host: ip,
-        open: true,
-        openPage: `http://${ip}:8080/`,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use:  [
-                    {
-                        loader: "./node_modules/html-webpack-plugin/lib/loader.js",
-                        options: {
-                            force: true,
-                        }
-                    },
-                    {
-                        loader: "string-replace-loader",
-                        options: {
-                            search:  "=\"(..\/assets\/[^\"]*)\"",
-                            replace: "=\"<%= require('$1').default %>\"",
-                            flags:   "g",
-                        }
-                    },
-                ]
-            }, {
-                test:    /\.tsx?$/,
-                use:     "babel-loader",
-                exclude: /node_modules/,
-            }, {
-                test: /\.(sass|scss|css)$/,
-                use:  [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    }, {
-                        loader:  "css-loader",
-                        options: {
-                            modules:   false,
-                            sourceMap: true,
+    const port = await getPort({ host: ip, port: getPort.makeRange(8080, 9000) });
+
+    return {
+        entry: {
+            main: "./src/ts/index.ts",
+        },
+        resolve: {
+            extensions: [".ts", ".tsx", ".js"],
+        },
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "[name].[chunkhash].js",
+            publicPath: "",
+        },
+        devtool: "source-map",
+        devServer: {
+            host: ip,
+            open: true,
+            openPage: `http://${ip}:${port}/`,
+            port,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    use: [
+                        {
+                            loader: "./node_modules/html-webpack-plugin/lib/loader.js",
+                            options: {
+                                force: true,
+                            }
                         },
-                    }, {
-                        loader:  "postcss-loader",
-                        options: {
-                            sourceMap: true,
+                        {
+                            loader: "string-replace-loader",
+                            options: {
+                                search: "=\"(..\/assets\/[^\"]*)\"",
+                                replace: "=\"<%= require('$1').default %>\"",
+                                flags: "g",
+                            }
                         },
-                    }, {
-                        loader:  "sass-loader",
-                        options: {
-                            sourceMap: true,
+                    ]
+                }, {
+                    test: /\.tsx?$/,
+                    use: "babel-loader",
+                    exclude: /node_modules/,
+                }, {
+                    test: /\.(sass|scss|css)$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        }, {
+                            loader: "css-loader",
+                            options: {
+                                modules: false,
+                                sourceMap: true,
+                            },
+                        }, {
+                            loader: "postcss-loader",
+                            options: {
+                                sourceMap: true,
+                            },
+                        }, {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true,
+                            },
                         },
-                    },
-                ],
-            }, {
-                test: /\.(png|svg|jpg|gif|ico|xml)$/,
-                use:  [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: "[name].[contenthash].[ext]",
+                    ],
+                }, {
+                    test: /\.(png|svg|jpg|gif|ico|xml)$/,
+                    use: [
+                        {
+                            loader: "file-loader",
+                            options: {
+                                name: "[name].[contenthash].[ext]",
+                            },
                         },
-                    },
-                ],
-            },
+                    ],
+                },
+            ],
+        },
+        plugins: [
+            new Dotenv(),
+            new MiniCssExtractPlugin({
+                filename: "styles.css",
+            }),
+            new HtmlWebpackPlugin({
+                scriptLoading: "defer",
+                inject: false,
+                hash: false,
+                template: "./src/html/index.html",
+            }),
+            new WebpackPwaManifest(require("./web_manifest")),
+            new WorkboxPlugin.GenerateSW(),
         ],
-    },
-    plugins: [
-        new Dotenv(),
-        new MiniCssExtractPlugin({
-            filename: "styles.css",
-        }),
-        new HtmlWebpackPlugin({
-            scriptLoading: "defer",
-            inject:        false,
-            hash:          false,
-            template:      "./src/html/index.html",
-        }),
-        new WebpackPwaManifest(require("./web_manifest")),
-        new WorkboxPlugin.GenerateSW(),
-    ],
-};
+    }
+})();
